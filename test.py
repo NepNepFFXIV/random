259211223b8477b633337cd040554e8f746934aa
Nep Nep <compsci88@gmail.com>
2020-04-29 08:09:04 -0700

    1st

---
+++ test.py
@@ -0,0 +1,24 @@
+import numpy as np
+import random
+import math
+from functools import reduce
+from operator import mul
+from decimal import Decimal
+from collections import namedtuple, Counter, deque, defaultdict, OrderedDict
+import string
+import re
+from itertools import islice, combinations, permutations
+import itertools
+from bisect import insort
+import sys
+import regex
+from time import time
+
+
+def dice(minimum, maximum):
+    seed = int(time())
+    ma = seed % maximum if maximum != 0 else 0
+    mi = seed % minimum if minimum != 0 else 0
+    return ma - mi
+
+print(dice(0, 2))
